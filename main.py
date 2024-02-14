d70f21466b573273e7168762b620259d4dba6ff4
joshuanewe <joshuanewe@gmail.com>
2024-02-13 19:48:24 -0800

    added the final book report for all character counts sorted

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ def main():
   text = get_book_text(book_path)
   num_of_words = get_num_words(text)
   letters = count_letters(text)
+
+  print(f"--- Begin report of {book_path} ---")
   print(f"{num_of_words} words found in document.")
-  print(letters)
+  sorted = sort_on(letters)
+  for letter in sorted:
+    print(f"The '{letter[0]}' character was found {letter[1]} times")
 
 def get_book_text(path):
   with open(path) as f:
@@ -21,6 +25,10 @@ def count_letters(text):
     if lowered in letters:
       letters[lowered] += 1
     else:
-      letters[lowered] = 1
+      if lowered.isalpha():
+        letters[lowered] = 1
   return(letters)
+
+def sort_on(dict):
+  return sorted(dict.items(), key=lambda x: x[1], reverse=True)
 main()
