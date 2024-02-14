caffd9836e4d97461854175494a8ee914c9d1117
joshuanewe <joshuanewe@gmail.com>
2024-02-13 19:27:02 -0800

    added a letter counter

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
   book_path = "/Users/joshuanewe/workspace/github.com/joshuanewe/bookbot/books/frankenstein.txt"
   text = get_book_text(book_path)
   num_of_words = get_num_words(text)
+  letters = count_letters(text)
   print(f"{num_of_words} words found in document.")
+  print(letters)
 
 def get_book_text(path):
   with open(path) as f:
@@ -12,4 +14,13 @@ def get_num_words(text):
   words = text.split()
   return len(words)
 
+def count_letters(text):
+  letters = {}
+  for letter in text:
+    lowered = letter.lower()
+    if lowered in letters:
+      letters[lowered] += 1
+    else:
+      letters[lowered] = 1
+  return(letters)
 main()
